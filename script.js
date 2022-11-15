12cd73d2a7a3eec2a545762cff9b41488a622320
Antonia Ekdahl <antoniaekdahl@gmail.com>
2022-11-15 13:22:01 +0100

    Add responsiv header

--- script.js
+++ script.js
@@ -12,22 +12,23 @@ xhr.onload = function getLatitudeLongitude() {
   let longitude = xhr.response[0].lon;
   xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=b688935d67e7556e5e058c9d5679aaee&units=metric", true);
   xhr.send();
-  xhr.onload = function getWeatherWithLatitudeLongitude() {
-    let weather = xhr.response.weather[0].main;
-    let temp = Math.round(xhr.response.main.temp);
-    printWeatherToScreen.innerHTML = temp +'C';
-    if (weather == 'Clouds') {
-      printWeatherToScreen.innerHTML += `<img src="img/moln.png" alt="Clouds" id="weatherIcon"></img>`;
-    } 
-    if (weather == 'Rain'){
-      printWeatherToScreen.innerHTML += `<img src="img/regn.png" alt="Rain" id="weatherIcon"></img>`;
-    } 
-    if (weather == 'Snow'){
-      printWeatherToScreen.innerHTML += `<img src="img/snö.png" alt="Snow" id="weatherIcon"></img>`;
-    }
-    if (weather == 'Clear'){
-      printWeatherToScreen.innerHTML += `<img src="img/sol.png" alt="Sun" id="weatherIcon"></img>`;
-    };
-  };
+      xhr.onload = function getWeatherWithLatitudeLongitude() {
+        let weather = xhr.response.weather[0].main;
+        let temp = Math.round(xhr.response.main.temp);
+        printWeatherToScreen.innerHTML = temp +'C';
+            if (weather == 'Clouds') {
+              printWeatherToScreen.innerHTML += `<img src="img/moln.png" alt="Clouds" id="weatherIcon"></img>`;
+            } 
+            if (weather == 'Rain'){
+              printWeatherToScreen.innerHTML += `<img src="img/regn.png" alt="Rain" id="weatherIcon"></img>`;
+            } 
+            if (weather == 'Snow'){
+              printWeatherToScreen.innerHTML += `<img src="img/snö.png" alt="Snow" id="weatherIcon"></img>`;
+            }
+            if (weather == 'Clear'){
+              printWeatherToScreen.innerHTML += `<img src="img/sol.png" alt="Sun" id="weatherIcon"></img>`;
+            };
+        };
 };
+
 
