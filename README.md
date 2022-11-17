d075c6a22714ef53156c1d35204cb4d5ce506bb1
Antonia Ekdahl <antoniaekdahl@gmail.com>
2022-11-17 15:40:05 +0100

    Add kommentarer i js  och förtydligande i README då inlämnad PFD inte beskriver projektet tydligt

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Inlämningsuppgift 2 - Väder
 
- 
+- En sida som med hjälp av AJAX hämtar först longitud och latitud från ett API.
+
+- Har hårdkodat så det är för Malmö, Sweden i URL. 
+
+- Tar sedan de variablerna för longitud och latitud och använder för att   
+  hämta temperatur och väder. 
+
+- Tar sedan de variablerna och skriver ut på skärmen. 
+
+- Har sedan lagt till bilder som visas om det är moln,sol,regn eller snö. 
--- script.js
+++ script.js
@@ -1,27 +1,33 @@
+// oklart om det är till någon nytta att jag skapat en egen funktion för det. Gjorde mest för att vara tydlig med namn. 
 function getData(){
-  fetchLatitudeLongitud();
+  getLatitudeLongitud();
 }
 
-async function fetchLatitudeLongitud(){
+//Gör en request för att hämta latidue och longitud från ett API.
+async function getLatitudeLongitud(){
 let response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Malmö,SE&limit=5&appid=e190806c15ff3407da1698ecb5b6cd98');
 let parsedBody = await response.json();
   if(response.ok){
   let latitude = parsedBody[0].lat;
   let longitude = parsedBody[0].lon;
-  fetchTempWeather("https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=b688935d67e7556e5e058c9d5679aaee&units=metric");
+//Använder sedan den datan och stoppar in i URL för att använda som argument i min funktion för att hämta väder och temperatur. 
+  getTempWeather("https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=b688935d67e7556e5e058c9d5679aaee&units=metric");
 }
 }
 
-async function fetchTempWeather(url){
+//Har skapat en function för väder som tar en parameter och får ut temperatur samt väder. 
+async function getTempWeather(url){
   response = await fetch(url);
   let parsedBody = await response.json();
   if(response.ok){
   let weather = parsedBody.weather[0].main;
   let temp = Math.round(parsedBody.main.temp);
+// Använder sedan datan jag fick ut som argument i min funktion som skriver ut det på skärmen.
   printToScreen(temp,weather);
   }
 }
 
+//Min funktion för att skriva ut på skärmen. 
 function printToScreen(temp,weather){
 let localWeatherTag = document.querySelector('#localWeather');
 localWeatherTag.innerHTML = temp +'C';
@@ -39,4 +45,5 @@ if (weather == 'Clear'){
 };
 }
 
+// Anropar funktionen. 
 getData();
